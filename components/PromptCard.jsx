4e76d81933db20eb1cdf97b5a461dae39fd89b3e
tushar-9015 <109728513+tushar-9015@users.noreply.github.com>
2023-09-20 03:27:08 +0530

    feat: create prompt

--- components/PromptCard.jsx
+++ components/PromptCard.jsx
@@ -1,9 +1,30 @@
+"use client"
 import React from 'react'
+import { useState } from 'react'
+import Image from 'next/image'
+import { useSession } from 'next-auth/react'
+import { usePathname, useRouter  } from 'next/navigation'
 
-const PromptCard = () => {
+const PromptCard = ({ post, handleTagClick, handleEdit, handleDelete }) => {
+
+  // console.log({post})
+  
   return (
-    <div>
-      
+    <div className='prompt_card'>
+      <div className='flex justify-between items-start gap-5'>
+        <div>
+          <Image 
+           src={post.creator.image}
+           alt='user_image'
+           width={40}
+           height={40}
+           className='rounded-full object-contain'
+          />
+          {/* <div className='flex flex-col'>
+            <h3>{post.creator.username}</h3>
+          </div> */}
+        </div>
+      </div>
     </div>
   )
 }
